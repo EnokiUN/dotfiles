c77fe25e18271e1ea5b8e8cc145e0edade27e32d
EnokiUN <enokiun@gmail.com>
2022-05-01 16:57:17 +0300

    ajoif

---
+++ init.el
@@ -0,0 +1,27 @@
+; About configuring emacs.
+; Some may say that taking the time to configure emacs is stupid, I mean it's true
+; that emacs is old and that the manual is unholyly huge but it's not about that.
+; Configuring emacs is about getting the most out of it. Additionally it's a display
+; of love and affection for your editor, not taking the time to read the 1 million+
+; lines of emacs source code just to be able to make a button look cooler is just
+; blasphemous and straight up ungrateful.
+
+; I'm not going to go into the details of how to configure emacs, but I'm going to
+; give you a few pointers.
+; 1. You can use the menu bar to access the emacs configuration menu, you should hide
+; the menu bar tho because it looks shit.
+; 2. You can find a gui configuration by running ``M-X customize`` but that defeats the
+; point of the whole display of love yada yada yada.
+; 3. You can read the manual but doing that is so retarded, the emacs manual is older than
+; the year 2000 and it's so huge that it's not even funny. (github copilot said that, don't
+; hold me accountable for it)
+; 4. You can read the source code but you're not going to be able to understand it and if you do
+; you'll be dead before you type a single character in your emacs config.
+; 5. Just read other people's config's ask around for what's good and what's not, google example stuff
+; and you'll be a-okay.
+; 6. ***Everyone has the same config, it's just slightly different every time and it scares me***.
+; 7. You can use the emacs tutorial to get a feel for emacs, it's a good way to get started. (I'm joking
+; don't even try it, just install evil mode, also this isn't related to config but shhhhh).
+
+; Now you understand why you should spend a net average of about 3 years of your life configuring
+; emacs and some ways to go about it.
